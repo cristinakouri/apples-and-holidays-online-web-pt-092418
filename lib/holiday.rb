13efe0053aa23c8462faa8176ef0070428fd814a
Cristina Kouri <cristina.isabel.kouri@gmail.com>
2018-10-12 22:57:29 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,7 +82,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, hash|
     binding.pry
-    season.to_s.capitalize
+    season.to_s.capitalize + ":"
   end
 end
 
