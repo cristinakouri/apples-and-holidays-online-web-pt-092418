19a85f44a754212135dfd7fe735718d24b505545
Cristina Kouri <cristina.isabel.kouri@gmail.com>
2018-10-12 22:25:19 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,47 +1,57 @@
 require 'pry'
 
-def second_supply_for_fourth_of_july(holiday_hash)
-  # given that holiday_hash looks like this:
-  # {
-  #   :winter => {
-  #     :christmas => ["Lights", "Wreath"],
-  #     :new_years => ["Party Hats"]
-  #   },
-  #   :summer => {
-  #     :fourth_of_july => ["Fireworks", "BBQ"]
-  #   },
-  #   :fall => {
-  #     :thanksgiving => ["Turkey"]
-  #   },
-  #   :spring => {
-  #     :memorial_day => ["BBQ"]
-  #   }
-  # }
-  # return the second element in the 4th of July array
-end
+   def second_supply_for_fourth_of_july(holiday_hash)
+    holiday_hash.each do |seasons, holidays|
+      holidays.each do |days, characteristics|
+        if days == :fourth_of_july
+          return characteristics[1]
+        end
+      end
+    end
+ end
+
+
+# holiday_hash is identical to the one above
+# add the second argument, which is a supply, to BOTH the
+# Christmas AND the New Year's arrays
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
-
+  holiday_hash.each do |season, holiday|
+    holiday.each do |day, things|
+      if day == :christmas || day == :new_years
+        things << supply
+      end
+    end
+  end
 end
 
+# again, holiday_hash is the same as the ones above
+# add the second argument to the memorial day array
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
+  holiday_hash.each do |season, holiday|
+    holiday.each do |day, things|
+      if day == :memorial_day
+        things << supply
+      end
+    end
+  end
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
+  holiday_hash.merge(season => {holiday_name => supply_array})
 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
+   holiday_hash.each do |season, holiday|
+    if season == :winter
+      return holiday.values.flatten
+    end
+  end
 
 end
 
